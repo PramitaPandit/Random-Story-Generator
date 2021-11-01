9fc5abd5ddef1fe96cdc3699b32b77ef3ee01c9d
PramitaPandit <78577352+PramitaPandit@users.noreply.github.com>
2021-11-01 11:19:12 -0500

    Add files via upload

---
+++ Random Story Generator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#importing random module
+import random
+
+#List of story inputs
+
+Time = ['Once Upon a time', 'A hundred years ago', 'Long before the modern world was created']
+Subject = ['a King', 'a mighty girl', 'the long footed giants', 'a group of friends']
+Place = ['a lonely palace', 'a dense forest', 'the mountains', 'the underworld beneath the sea']
+
+#Selecting an item from each list and concatenating them
+print(random.choice(Time) + ' ' + random.choice(Subject) + ' lived in ' + random.choice(Place))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
